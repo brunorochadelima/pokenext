dad272953c4e6416100e8dbf1c5c09c2ac0a9974
Bruno Rocha de Lima <brunorocha33@live.com>
2022-10-07 11:01:33 -0300

    criando cards home

---
+++ pages/components/Card.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Card({ pokemon }) {
+  return (
+    <div>
+      <Image
+        src={`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${pokemon.id}.png`}
+        width="120"
+        height="120"
+        alt={pokemon.name}
+      />
+      <h3>{pokemon.name}</h3>
+      <p>id:{pokemon.id}</p>
+      <Link href={`/pokemon/${pokemon.id}`}>Detalhes</Link>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Card from "./components/Card";
 
 export async function getStaticProps() {
   const limit = 250;
@@ -10,7 +11,19 @@ export async function getStaticProps() {
 
   //ad pokemon index
   response.results.forEach((item, index) => {
+    // cria id
     item.id = index + 1;
+
+    // converte id para ter no mínimo 3 digitos
+    var numberWithZeroes = String(item.id);
+    var counter = numberWithZeroes.length;
+
+    while (counter < 3) {
+      numberWithZeroes = "0" + numberWithZeroes;
+      counter++;
+    }
+
+    item.id = numberWithZeroes;
   });
 
   return {
@@ -24,7 +37,7 @@ export default function Home({ pokemons }) {
       <h1 className={styles.titulo}>Título</h1>
       <div className={styles.container_pokemon}>
         {pokemons.map((item, index) => (
-          <p key={item.id}>{item.name}</p>
+          <Card key={item.id} pokemon={item} />
         ))}
       </div>
     </div>
