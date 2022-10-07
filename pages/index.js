cec4d635f5269439f7f30abde9545e221eead9fe
Bruno Rocha de Lima <brunorocha33@live.com>
2022-10-07 09:28:50 -0300

    criando página home

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,31 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export async function getStaticProps() {
+  const limit = 250;
+
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`);
+  const response = await data.json();
+
+  //ad pokemon index
+  response.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: { pokemons: response.results },
+  };
+}
+
+export default function Home({ pokemons }) {
   return (
-    <div className={styles.container}>
-      <main>
-        <h1>Título da Home</h1>
-      </main>
+    <div>
+      <h1 className={styles.titulo}>Título</h1>
+      <div className={styles.container_pokemon}>
+        {pokemons.map((item, index) => (
+          <p key={item.id}>{item.name}</p>
+        ))}
+      </div>
     </div>
   );
 }
